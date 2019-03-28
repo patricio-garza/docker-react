86976155c4216c90ceb8cb36a11b22b6191791e8
Garza <patricio.garza@sony.com>
2019-03-28 11:47:22 -0700

    Initial Commmit

---
+++ build/precache-manifest.eda8ce7c7b1394aa83a8aaf7a7fc272e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2a68109613ba45fdc8f",
+    "url": "/static/css/main.e2df2d5f.chunk.css"
+  },
+  {
+    "revision": "f2a68109613ba45fdc8f",
+    "url": "/static/js/main.69b15cd4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fa8b087662f200b63b8adfb13e2d7ac9",
+    "url": "/index.html"
+  }
+];
